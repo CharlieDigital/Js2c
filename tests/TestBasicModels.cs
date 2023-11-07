7e726acea35c53f05bcd930b33820e008d4b2179
Charles Chen <chen.charles.c@gmail.com>
2023-11-06 21:50:13 -0500

    Added integer notes

--- tests/TestBasicModels.cs
+++ tests/TestBasicModels.cs
@@ -7,11 +7,13 @@ public class TestBasicModels {
   public void Can_Use_Simple_Models() {
     var sandra = new Person {
       FirstName = "Sandra",
-      LastName = "Smith"
+      LastName = "Smith",
+      Age = 24m
     };
 
     Assert.Equal("Sandra", sandra.FirstName);
     Assert.Equal("Smith", sandra.LastName);
+    Assert.Equal(24m, sandra.Age);
   }
 
   [Fact]
@@ -21,21 +23,23 @@ public class TestBasicModels {
     Assert.NotNull(adam);
     Assert.Equal("Adam", adam.FirstName);
     Assert.Equal("Ng", adam.LastName);
+    Assert.Equal(0, adam.Age);
   }
 }
 
-  /// <summary>
-  /// This partial class exposes a constant JSON expression which is used to generate
-  /// the class.
-  /// </summary>
-  [JsonSource(Json)]
-  public partial class Person {
+/// <summary>
+/// This partial class exposes a constant JSON expression which is used to generate
+/// the class.
+/// </summary>
+[JsonSource(Json)]
+public partial class Person {
 
-    /// <summary>
-    /// This is a reference JSON string that we'll use to create the class.
-    /// </summary>
-    internal const string Json = @"{
-      ""firstName"" : ""Charles"",
-      ""lastName"" : ""Chen""
-    }";
-  }
+  /// <summary>
+  /// This is a reference JSON string that we'll use to create the class.
+  /// </summary>
+  internal const string Json = @"{
+    ""firstName"" : ""Charles"",
+    ""lastName"" : ""Chen"",
+    ""age"": 21
+  }";
+}
